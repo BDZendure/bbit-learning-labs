9688c3e7015321bc8a03f17e01709b4824b3f8d5
Blake Zhang <109260122+BDZendure@users.noreply.github.com>
2026-03-12 02:17:39 +0000

    finished topic exchange publish

--- tech_lab_on_campus/market_watch/topic_exchange/publish.py
+++ tech_lab_on_campus/market_watch/topic_exchange/publish.py
@@ -24,8 +24,7 @@ def main(ticker: str, price: float, sector: str) -> None:
     #
     #                       WRITE CODE HERE!!!
     #
-
-
+    routingKey = self.ticker + "." + self.sector
     producer = mqProducer(routing_key=routingKey,exchange_name="Tech Lab Topic Exchange")
 
 
@@ -33,7 +32,8 @@ def main(ticker: str, price: float, sector: str) -> None:
     #
     #                       WRITE CODE HERE!!!
     #
-    
+
+    message = self.ticker + " price is now $" + self.price
     
     producer.publishOrder(message)
 
@@ -43,5 +43,10 @@ if __name__ == "__main__":
     #
     #                       WRITE CODE HERE!!!
     #
+
+    # note: sys.argv[0] is file
+    self.ticker = sys.argv[1]
+    self.price = sys.argv[2]
+    self.sector = sys.argv[3]
 
     sys.exit(main(ticker,price,sector))
